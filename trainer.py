9dc843cbcd975bde3bb362587b47159c68873418
mgrenander <matt.grenander@gmail.com>
2018-04-12 12:59:09 -0400

    fixing bug in trainer

--- trainer.py
+++ trainer.py
@@ -6,19 +6,20 @@ import pickle
 import preprocessing
 
 print("Downloading data")
-train, dev, test, answers, inputs = preprocessing.get_data()
+train_iter, dev_iter, test_iter, answers, inputs = preprocessing.get_data()
 
 # TODO: modify vocab size with actual vocab size
 print("Creating model")
 model = LSTMSentiment(embedding_dim=300, hidden_dim=128, vocab_size=300, label_size=2)
 loss_function = nn.CrossEntropyLoss()
-optimizer = optim.SGD(model.parameters(), lr=0.001)
+optimizer = optim.Adam(model.parameters(), lr=0.001)
 
 print("Beginning training")
 for epoch in range(5):
+    train_iter.init_epoch()
     print("Epoch {}".format(epoch))
-    for batch_idx, batch in enumerate(train):
-        model.zero_grad()
+    for batch_idx, batch in enumerate(train_iter):
+        model.train(); optimizer.zero_grad()
         model.hidden = model.init_hidden()
 
         # Forward pass
@@ -31,8 +32,9 @@ for epoch in range(5):
 
 # calculate accuracy on validation set
 n_test_correct, test_loss = 0, 0
-for dev_batch_idx, dev_batch in enumerate(test):
-    answer = model(test)
+for dev_batch_idx, dev_batch in enumerate(test_iter):
+    answer = model(test_iter)
     n_test_correct += (torch.max((answer, 1))[1].view(dev_batch.label.size()).data == dev_batch.label.data).sum()
 
-dev_acc = 100. * n_test_correct / len(dev)
+test_acc = 100. * n_test_correct / len(test_iter)
+print(test_acc)
