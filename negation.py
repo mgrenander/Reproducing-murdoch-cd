e0c71e398bed06407d0e6c372357b88835237124
mgrenander <matt.grenander@gmail.com>
2018-04-18 15:14:14 -0400

    testing CD now

--- negation.py
+++ negation.py
@@ -2,20 +2,30 @@ from ContextualDecomposition import CD
 import torch
 from torchtext import data, datasets
 
+
+def filter_neg(example):
+    pass
+
+
 # Load model and data
-DEVICE = 0
 print("Loading model")
-model = torch.load("model.pt")
+model = torch.load("model.pt", map_location=lambda storage, loc: storage)
 
 print("Fetching data and creating splits")
 inputs = data.Field(lower='preserve-case')
 answers = data.Field(sequential=False, unk_token=None)
-train, dev, test = datasets.SST.splits(inputs, answers, fine_grained = False, train_subtrees = True, filter_pred=lambda ex: ex.label != 'neutral')
+train, dev, test = datasets.SST.splits(inputs, answers, fine_grained = False, train_subtrees = True, filter_pred=None)
 inputs.build_vocab(train, dev, test, vectors="glove.6B.100d")
 answers.build_vocab(train)
-train_iter, dev_iter, test_iter = data.BucketIterator.splits((train, dev, test), batch_size=1, repeat=False, device=DEVICE)
+vocab = inputs.vocab
 
-print("Computing CD scores")
-for batch in train_iter:
-    print(CD(batch, model, 0, len(batch.text)-1))
-    break
+ok = [vocab.stoi[i] for i in test[0].text]
+print(CD(ok, model, 0, len(ok)-1))
+
+
+# print("Computing CD scores")
+# for ex in train:
+
+    # filtered_tuples = filter_neg(ex)
+    # print(filtered_tuples)
+    # break
